c97e830a06f861d62fa531597d99e6fd9f966c0c
Yuliia Synytsia <lm.julia92@gmail.com>
2019-05-18 12:51:19 -0700

    make Peacemakr conform to protocol

---
+++ SDK/Metadata.swift
@@ -0,0 +1,28 @@
+//
+//  Metadata.swift
+//  Peacemakr-iOS
+//
+//  Created by Yuliia Synytsia on 5/18/19.
+//  Copyright © 2019 Peacemakr. All rights reserved.
+//
+
+import Foundation
+import CoreCrypto
+
+class Metadata {
+  static let shared = Metadata()
+  
+  init(){}
+  
+  var version: String {
+    if let bundleInfo = Bundle(for: type(of: self)).infoDictionary,
+      let shortVersion = bundleInfo["CFBundleShortVersionString"] as? String,
+      let bundleVersion = bundleInfo["CFBundleVersion"] as? String {
+      return "\(shortVersion).\(bundleVersion)"
+    } else {
+      return "<Unknown Peacemakr SDK version>"
+    }
+  }
+  
+}
+
